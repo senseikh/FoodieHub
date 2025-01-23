1756e3a8c56b88190002436d53d774d9e691d123
SenseiKh <hillarykisera@gmail.com>
2025-01-23 18:15:46 +0300

    Updated some Of the codde

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipes,Category,Comment,Tag,UserProfile
 
 # Register your models here.
+admin.site.register(Recipes)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(UserProfile)
