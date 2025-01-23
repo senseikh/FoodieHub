f4eab5c2da1e47586ce3e5a07e2f3b591e1baaf2
SenseiKh <hillarykisera@gmail.com>
2025-01-23 16:50:17 +0300

    Urls update

--- api/urls.py
+++ api/urls.py
@@ -19,13 +19,14 @@ urlpatterns = [
     path("users/", CreateUserView.as_view(), name="create_user"),
     path("recipe/", views.CreateRecipeView.as_view(), name="recipe creattion"),
     path("recipe/delete/<int:pk>/", views.RecipeDelete.as_view(), name="Recipe deletion"),
-    path("recipes/list/", RecipeListView.as_view(), name="list_recipes"),
+    path("recipes/<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
+    path("recipes/<int:pk>/update/", RecipeUpdateView.as_view(), name="recipe_update"),
+    # path("recipes/list/", RecipeListView.as_view(), name="list_recipes"),
     path("categories/", CategoryListCreateView.as_view(), name="list_create_categories"),
     path("categories/<int:pk>/", CategoryDetailView.as_view(), name="category_detail"),
     path("tags/", TagListCreateView.as_view(), name="list_create_tags"),
     path("tags/<int:pk>/", TagDetailView.as_view(), name="tag_detail"),
     path("recipes/<int:recipe_id>/comments/", CommentListCreateView.as_view(), name="list_create_comments"),
     path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="delete_comment"),
-    path("recipes/<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
-    path("recipes/<int:pk>/update/", RecipeUpdateView.as_view(), name="recipe_update"),
+    
 ]
