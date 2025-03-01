1b00aba41bd31c9b012dbca2a14f9a2b9f131bd7
SenseiKh <hillarykisera@gmail.com>
2025-03-01 19:32:29 +0300

    jk

--- FoodHub/settings.py
+++ FoodHub/settings.py
@@ -108,10 +108,10 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend'
-]
+# AUTHENTICATION_BACKENDS = [
+#     'django.contrib.auth.backends.ModelBackend',
+#     'allauth.account.auth_backends.AuthenticationBackend'
+# ]
 
 
 DATABASES = {
